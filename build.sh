f7a0659a49affc6833dbe20deb78817511ac0d88
James Byatt <grumpyjames@gmail.com>
2018-03-18 18:15:52 +0000

    add site index and a hilarious build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+mkdir -p out/sites
+for file in `ls sites/*.md`; do
+    outfile="${file%.*}"
+    pandoc -f markdown -t html $file > out/$outfile.html
+done
+
+cp index-header.html out/index.html
+for file in `ls out/sites/*html`; do
+    site_name="$(basename "${file%.*}")"
+    human_site_name="$(echo $site_name | tr '_' ' ')"
+    echo "<li><a href=\"sites/$site_name.html\">$human_site_name</a>" >> out/index.html
+done
+cat index-footer.html >> out/index.html
