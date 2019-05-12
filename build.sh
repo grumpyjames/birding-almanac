4c67441045b5b9c700a1a14b2017dbcb512db868
James Byatt <grumpyjames@gmail.com>
2019-05-12 17:34:54 +0100

    get closer to a one-line 'ship'

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 mkdir -p out/sites
 for file in `ls sites/*.md`; do
     outfile="${file%.*}"
-    cp site-header.html out/$outfile.html
+    cp index-header.html out/$outfile.html
     pandoc -f markdown -t html $file >> out/$outfile.html
-    cat site-footer.html >> out/$outfile.html
+    cat index-footer.html >> out/$outfile.html
 done
 
 cp index-header.html out/index.html
@@ -13,3 +13,5 @@ for file in `ls out/sites/*html`; do
     echo "<li><a href=\"sites/$site_name.html\">$human_site_name</a>" >> out/index.html
 done
 cat index-footer.html >> out/index.html
+
+cp *.png out/
